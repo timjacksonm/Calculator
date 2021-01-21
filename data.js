58e38a93b1420245e2cefcd84f1c3e32a5978854
Tim Jackson <timjacksonm@gmail.com>
2021-01-21 10:03:59 -0600

    created functions for add, subtract, multiply, divide & operate

--- data.js
+++ data.js
@@ -1,4 +1,22 @@
-function test(a, b) {
-    console.log(a+b);
-}
-test(1,2);
+const add = (a, b) => a+b;
+
+const subtract = (a, b) => a-b;
+
+const multiply = (a, b) => a*b;
+
+const divide = (a, b) => a/b;
+
+function operate(operator, a, b){
+    const num1 = a;
+    const num2 = b;
+
+    if(operator === '+'){
+        return add(num1,num2);
+       }else if(operator === '-') {
+        return subtract(num1,num2);
+       }else if(operator === '*') {
+        return multiply(num1,num2);
+       }else if(operator === '/') {
+        return divide(num1,num2);
+       }
+};
