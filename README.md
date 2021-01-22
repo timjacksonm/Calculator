df9f14d20619465419f6c9502b26bee7d16fba4c
Tim Jackson <timjacksonm@gmail.com>
2021-01-22 15:07:45 -0600

    created a few new functions that now make the calculator work! you can type in a math equation using any of the four operators and the solution will output on the screen

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Calculator
 
 ## Goals:
-- [ ] Your calculator needs to contain functions for all basic math operators you typically find on simple calculators.`+ - * /`
-- [ ] Create a new function `operate` that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
-- [ ] Create a basic HTML calculator with buttons for each digit and each of the above functions.
+- [✔️] Your calculator needs to contain functions for all basic math operators you typically find on simple calculators.`+ - * /`
+- [✔️] Create a new function `operate` that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+- [✔️] Create a basic HTML calculator with buttons for each digit and each of the above functions.
 - [ ] Add an "equals" & "clear" button.
-- [ ] Create the functions that populate the display when you click the number buttons.
-- [ ] Make the calculator work! Use previous DOM lessons learned and the functions you just created.
+- [✔️] Create the functions that populate the display when you click the number buttons.
+- [✔️] Make the calculator work! Use previous DOM lessons learned and the functions you just created.
 
 **Watch out for and fix these bugs if they show up in your code:**
 
@@ -18,7 +18,7 @@
 **Extra Credit**
 
 - [ ] Add a ` . ` button and let users input decimals!
-- [ ] Make the calculator look nice!
+- [✔️] Make the calculator look nice!
 - [ ] Add a `backspace` button.
 - [ ] Add keyboard support!
 
--- data.js
+++ data.js
@@ -7,60 +7,76 @@ const multiply = (a, b) => a*b;
 const divide = (a, b) => a/b;
 
 function operate(operator, a, b){
-    const num1 = a;
-    const num2 = b;
+    const num1 = Number(a);
+    const num2 = Number(b);
+
+    const screenDialog = document.querySelector('#text');
 
     if(operator === '+'){
-        return add(num1,num2);
+        screenDialog.textContent = add(num1,num2);
        }else if(operator === '-') {
-        return subtract(num1,num2);
+        screenDialog.textContent = subtract(num1,num2);
        }else if(operator === '*') {
-        return multiply(num1,num2);
+        screenDialog.textContent = multiply(num1,num2);
        }else if(operator === '/') {
-        return divide(num1,num2);
+        screenDialog.textContent = divide(num1,num2);
        }
 };
 
 function allButtons() {
     const numOne = document.querySelector('#one');
-    numOne.addEventListener('click', () => { screenDialog(numOne.textContent)});
+    numOne.addEventListener('click', () => { logDataArray(numOne.textContent), updateScreenText()});
     const numTwo = document.querySelector('#two');
-    numTwo.addEventListener('click', () => { screenDialog(numTwo.textContent)});
+    numTwo.addEventListener('click', () => { logDataArray(numTwo.textContent), updateScreenText()});
     const numThree = document.querySelector('#three');
-    numThree.addEventListener('click', () => { screenDialog(numThree.textContent)});
+    numThree.addEventListener('click', () => { logDataArray(numThree.textContent), updateScreenText()});
     const numFour = document.querySelector('#four');
-    numFour.addEventListener('click', () => { screenDialog(numFour.textContent)});
+    numFour.addEventListener('click', () => { logDataArray(numFour.textContent), updateScreenText()});
     const numFive = document.querySelector('#five');
-    numFive.addEventListener('click', () => { screenDialog(numFive.textContent)});
+    numFive.addEventListener('click', () => { logDataArray(numFive.textContent), updateScreenText()});
     const numSix = document.querySelector('#six');
-    numSix.addEventListener('click', () => { screenDialog(numSix.textContent)});
+    numSix.addEventListener('click', () => { logDataArray(numSix.textContent), updateScreenText()});
     const numSeven = document.querySelector('#seven');
-    numSeven.addEventListener('click', () => { screenDialog(numSeven.textContent)});
+    numSeven.addEventListener('click', () => { logDataArray(numSeven.textContent), updateScreenText()});
     const numEight = document.querySelector('#eight');
-    numEight.addEventListener('click', () => { screenDialog(numEight.textContent)});
+    numEight.addEventListener('click', () => { logDataArray(numEight.textContent), updateScreenText()});
     const numNine = document.querySelector('#nine');
-    numNine.addEventListener('click', () => { screenDialog(numNine.textContent)});
+    numNine.addEventListener('click', () => { logDataArray(numNine.textContent), updateScreenText()});
     const numZero = document.querySelector('#zero');
-    numZero.addEventListener('click', () => { screenDialog(numZero.textContent)});
+    numZero.addEventListener('click', () => { logDataArray(numZero.textContent), updateScreenText()});
     const addV = document.querySelector('#add');
-    addV.addEventListener('click', () => { screenDialog(addV.textContent)});
+    addV.addEventListener('click', () => { logDataArray(addV.textContent, addV.getAttribute('value')), screenShowOpt()});// passing two arguements because my textContent won't work on operate() for division and multiply ex (5 'X' 5 =NAN, but 5 '*' 5 =25)
     const subtractV = document.querySelector('#subtract');
-    subtractV.addEventListener('click', () => { screenDialog(subtractV.textContent)});
+    subtractV.addEventListener('click', () => { logDataArray(subtractV.textContent, subtractV.getAttribute('value')), screenShowOpt()});// add and subtract I just added values to aswell even though it wasn't really needed.
     const multiplyV = document.querySelector('#multiply');
-    multiplyV.addEventListener('click', () => { screenDialog(multiplyV.textContent)});
+    multiplyV.addEventListener('click', () => { logDataArray(multiplyV.textContent, multiplyV.getAttribute('value')), screenShowOpt()}); // the logDataArray() function populates the screen with arg[0] and the if statement uses arg[1]
     const divideV = document.querySelector('#divide');
-    divideV.addEventListener('click', () => { screenDialog(divideV.textContent)});
+    divideV.addEventListener('click', () => { logDataArray(divideV.textContent, divideV.getAttribute('value')), screenShowOpt()}); // --^
 
     const powerB = document.querySelector('#powerButton');
     powerB.addEventListener('click', () => { turnOnScreen()});
-}
-function screenDialog() {
-    let argArray = Array.from(arguments); // stop point for the night
-    console.log(argArray)
-    results = argArray[0].toString();
-    const screenDialog = document.querySelector('#text');
 
-    screenDialog.textContent = results;
+    const equalsB = document.querySelector('#equals');
+    equalsB.addEventListener('click', () => { equalsKeyPress()});
+
+
+}
+function logDataArray() {
+    // const screenDialog = document.querySelector('#text');
+    // screenDialog.textContent = arguments[0];
+
+    let intiger = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    let operator = ['+', '-', '*', '/']
+
+    if(intiger.includes(Number(arguments[0]))) {
+        if(operateInput.includes('nothing')) {
+            numBeforeOp.push(arguments[0]);
+        }else
+        numAfterOp.push(arguments[0]);
+    }else if(operator.includes(arguments[1])) {
+        operateInput.pop();
+        operateInput.push(arguments[1]);
+    }
 }
 function turnOnScreen() {
     const selectBackroundColor = document.querySelector('#screen');
@@ -68,6 +84,28 @@ function turnOnScreen() {
     selectBackroundColor.classList.add('screenStyle');
 
 }
-allButtons();
+function updateScreenText () {
+    const screenDialog = document.querySelector('#text');
 
+    if(operateInput.includes('nothing')) {
+    screenDialog.textContent = numBeforeOp.join('');
+    }else
+    screenDialog.textContent = numAfterOp.join(''); // can shorten since its boolean true false
+}
+function screenShowOpt() {
+    const screenDialog = document.querySelector('#text');
+
+    screenDialog.textContent = operateInput;
+}
+allButtons();
+let numBeforeOp = [];
+let operateInput = ['nothing'];
+let numAfterOp = [];
+
+function equalsKeyPress() {
+    let numPassA = numBeforeOp.join('');
+    let numPassB = numAfterOp.join('');
+
+operate(operateInput[0], numPassA, numPassB);
+}
 
